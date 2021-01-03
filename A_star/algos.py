da1c5a6aa3e6cd4d7dc0707ebffca17329ea761a
jr2bg <j.arturo.b.g@gmail.com>
2021-01-02 23:00:28 -0600

    start A* and final details to Dijkstra

--- A_star/algos.py
+++ A_star/algos.py
@@ -58,7 +58,20 @@ class Node():
 
 
     # add the relaxed nodes to Q
-    def relax(self, adj_node, weight_s2ad, Q):
+    def relax(self, adj_node, weight_s2ad, opn, h):
+        '''
+        método para encontrar la relajación de los nodos adjacentes a self que
+        considera ya la heurística a emplear
+
+        adj_node -> un nodo adyacente a self
+        weight_s2ad -> peso de la arista desde self a adj_node
+        opn -> heap correspondiente a los pesos acumulados para las aristas
+        h -> heurística, en este caso emplearemos la distancia de Manhattan
+        '''
+
+        # distancia estimada
+        estimated = h(r_target, c_target, r_origin, c_origin)
+        
         # adj_node is a node in nodes_dist_dic
         #  distancia nodo adj >   distancia hasta ese nodo + peso
         if adj_node.d  > self.d + weight_s2ad:
@@ -67,17 +80,23 @@ class Node():
             #print(adj_node.coords())
             #print(Q)
             #print(Q)
-            heapq.heappush(Q, (t, adj_node.coords()))
+            heapq.heappush(opn, (t, adj_node.coords()))
             adj_node.pre = self
-        return Q
+        return opn
 
-    def relaxing_adjNodes(self, Q):
-        #print(self.nodes_dist_dic)
+    def relaxing_adjNodes_aS(self, opn , cls):
+        '''
+        método para "relajar" los nodos adjacentes a self
+
+        opn -> nodos aun no evaluados
+        cls -> nodos ya evaluados
+        '''
+        #
         for key, val in self.nodes_dist_dic.items():
-            Q = self.relax(key, val, Q)
+            opn = self.relax(key, val, opn)
             #print(val)
             #print(key.pre.d)
-        return Q
+        return opn
 
 
 class Grid:
@@ -101,9 +120,9 @@ class Grid:
         self.n_cols = len(data[0])
 
 
-        #  obstáculos fijos -> 0; libres -> 1; target -> 2 ; obstáculos móvles -> 3
+        #  obstáculos fijos -> 0; libres -> 30; target -> 2 ; obstáculos móvles -> 3
 
-        self.display =[[1 for j in range(self.n_cols)] for i in range(self.n_rows)]
+        self.display =[[30 for j in range(self.n_cols)] for i in range(self.n_rows)]
 
         # generación del espacio
         self.generate_space()
@@ -184,14 +203,33 @@ class Grid:
             #print()
 
     def init_single_source_grid(self, row_p, col_p):
-        # iteración sobre todos los nodos para inicializar Dijkstra
+        '''
+        iteración sobre todos los nodos para inicializar Dijkstra / A*
+
+        row_p -> fila de origen
+        col_p -> columna de origen
+        '''
         for r in range(self.n_rows):
             for c in range(self.n_cols):
                 # se inicializa el espacio para cada nodo
                 self.space[r][c].init_single_source_node(row_p, col_p)
 
+
+def manhattan(r_target, c_target, r_origin, c_origin):
+    '''
+    distancia de Manhattan
+    empleada como heurística para el algoritmo A*
+    '''
+    return abs(r_target - r_origin) + abs(c_target - c_origin)
+
 # función para remover los nodos nodo de un heap
 def remove_nodes(Q, nodo):
+    '''
+    función para remover los nodos nodo de un heap
+
+    Q -> heap
+    nodo -> nodo a remover
+    '''
     l = []
     # identifica todos los nodos correspondientes
     for i in range(len(Q)):
@@ -206,60 +244,103 @@ def remove_nodes(Q, nodo):
     heapq.heapify(Q)
     return Q
 
-def get_path(finishedDijkstra, r_target, c_target):
-    # función para tener el path desde el origen hasta el destino
+def get_path(finishedAS, r_target, c_target):
+    '''
+    función para tener el path desde el origen hasta el destino
+
+    finishedAS -> instancia de Grid que ya ha pasado por A* y terminado
+    r_target -> fila de la ubicación del destino
+    c_target -> columna de la ubicación del destino
+    '''
     pt = [(r_target,c_target)]
-    nodito = finishedDijkstra.space[r_target][c_target]
+    nodito = finishedAS.space[r_target][c_target]
     while nodito.pre:
         # en posición 0 insertamos el nodo previo
         nodito = nodito.pre
         pt.insert(0, nodito.coords())
     return pt
 
-def show_path(pt, finishedDijkstra):
+def show_path(pt, finishedAS):
     for par in pt:
-        finishedDijkstra.display[par[0]][par[1]] = 4
-    plt.imshow(finishedDijkstra.display)
+        # path color : 5
+        finishedAS.display[par[0]][par[1]] = 5
+    plt.imshow(finishedAS.display)
     plt.show()
 
 
-def dijkstra(generatedGrid, r_target, c_target, r_origin, c_origin):
-    # empty heap
+def a_star(generatedGrid, r_target, c_target, r_origin, c_origin):
+    '''
+    Implementación del algoritmo A*
+
+    generatedGrid -> instancia de Grid que tiene ya tanto el mapa como el origen
+    r_target -> fila del target (0-based)
+    c_target -> columna del target (0-based)
+    r_origin -> fila del origen (0-based)
+    c_origin -> columna del origen (0-based)
+    '''
+    # row number and column number of the grid
+    t_rows = len(generatedGrid.space)
+    t_cols = len(generatedGrid.space[0])
+
     # nodo de origen
     s_node = generatedGrid.space[r_origin][c_origin]
+
     #generatedGrid.display[r_origin][c_origin] = 3
     #generatedGrid.display[r_target][c_target] = 4
-    Q = [(s_node.d, s_node.coords())]
-    heapq.heapify(Q)
-    S = []
+    #Q = [(s_node.d, s_node.coords())]
+    #heapq.heapify(Q)
+    #S = []
 
+    # initialize evaluated nodes (cls) and not yet evaluated nodes (opn)
+    opn = [(s_node.d, s_node.coords())]
+    cls = []
+
+    # heapify the opn list to get the smallest element
+    heapq.heapify(opn)
+
+    # list to get the animations
     list_anims = []
+
+    # figure initialization
     fig = plt.figure()
-    while len(Q) > 0:
-        dist, cds = heapq.heappop(Q)
-        #print(cds)
+
+    while len(opn) > 0:
+
+        # get the minimum distance of the heap
+        dist, cds = heapq.heappop(opn)
+
+        # add "node" to cls
+        cls.append(cds)
+
+        # last extracted node's information
         nodito = generatedGrid.space[cds[0]][cds[1]]
 
-
-        generatedGrid.display[cds[0]][cds[1]] = 2
+        # explored color : 3
+        generatedGrid.display[cds[0]][cds[1]] = 3
         img = plt.imshow(generatedGrid.display)
         list_anims.append([img])
         #plt.pause(0.01)
         #plt.show()
 
-        S.append(cds)
+        #S.append(cds)
         if cds == (r_target, c_target):
             break
-        #print(S)
-        Q = nodito.relaxing_adjNodes(Q)
+
+
+        # considering only those nodes NOT YET EVALUATED
+        opn = nodito.relaxing_adjNodes_aS(opn, cls)
         #print(Q)
         # removemos nodos repetidos, esto es, nodos en S
         for cons_node in S:
             #print(cons_node)
-            Q = remove_nodes(Q, cons_node)
+            opn = remove_nodes(opn, cons_node)
+
+    # create the animation from the list of figures
     ani = animation.ArtistAnimation(fig, list_anims, interval=100, blit=True, repeat_delay=1000)
     writergif = animation.PillowWriter(fps=5)
     ani.save('filename.gif',writer=writergif)
+
+    # get the list of the previous nodes
     return S, list_anims
 
 #nodo1 = Node(0,0)
